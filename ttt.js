cfdb3f76a8380964ca04ac1e825d51921babb199
Aryaman Tandon <tandon.aryaman1@gmail.com>
2023-05-28 19:00:55 +0530

    changed

--- ttt.js
+++ ttt.js
@@ -4,13 +4,13 @@ let isgameover= false;
 let finishaudio=new Audio("music.mp3");
 let clickmusic=new Audio("gameover.mp3");
 //function to change the turn 
-let changeturn=() =>{
+const changeturn=() =>{
 return turn === "X"?"0":"X";
 }
 // function to check for a win 
-let checkWin=() =>{
+const checkWin=() =>{
     let boxtext = document.getElementsByClassName("text");
-    const win = [
+    let win = [
         [0, 1, 2, 5, 5, 0],
         [3, 4, 5, 5, 15, 0],
         [6, 7, 8, 5, 25, 0],
@@ -19,34 +19,36 @@ let checkWin=() =>{
         [2, 5, 8, 15, 15, 90],
         [0, 4, 8, 5, 15, 45],
         [2, 4, 6, 5, 15, 135],
-    ];
-    win.forEach((e)=> {
-            if ((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[1]].innerText === boxtext[e[2]].innerText) && (boxtext[e[0]] !== "")) {
-                clickmusic.play();
-                isgameover = true;
+    ]
+    win.forEach(e=> {
+            if ((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[1]].innerText === boxtext[e[2]].innerText) && (boxtext[e[0]].innerText !== "")) {
                 finishaudio.play();
-                document.querySelector('.info').innerText = boxtext[e[0]].innerText + "won";
-                document.querySelector(".dance").getElementsByTagName('img')[0].style.width = "206px";
-                document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
-                document.querySelector(".line").style.width = "20vw";
-            }
-        })          
-}
+                isgameover=true;
+        document.querySelector('.info').innerText = (boxtext[e[0]].innerText + "won");
+        document.querySelector(".dance").getElementsByTagName("img")[0].style.visibility="visible";
+        document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
+        document.querySelector(".line").style.width = "20vw";
+    }
+               
+                
+        })  }        
+    
+
+
 //main logic 
 
 let boxes=document.getElementsByClassName("box");
 Array.from(boxes).forEach(element =>{
     let boxtext= element.querySelector(".text");
-    element.addEventListener('click',()=>{
-        if(boxtext.innerText=== ''){
+    element.addEventListener("click",()=>{
+        if(boxtext.innerText===""){
             boxtext.innerText= turn;
             turn = changeturn(); 
+            clickmusic.play();
             checkWin();
-            
-            
             if(!isgameover){
-            document.getElementsByClassName("info")[0].innerText=`"Turn for" + ${turn}`
-        }
+            document.getElementsByClassName("info")[0].innerText=`"Turn for" ${turn}`}
+         console.log("123456");
 
         }
     } )
@@ -55,11 +57,12 @@ let reset = document.querySelector("#reset");
 reset.addEventListener('click',()=>{
     let boxtext=document.querySelectorAll(".text");
     Array.from(boxtext).forEach(element=>{
-        element.innerText= ""
+        element.innerText= "";
     });
     turn="X";
-    document.getElementsByClassName("info")[0].innerText=`"Turn for" + ${turn}`;
-    document.querySelector(".dance").getElementsByTagName('img')[0].style.width="0px";
+    finishaudio.pause();
+    document.getElementsByClassName("info")[0].innerText=`"Turn for"  ${turn}`;
+    document.querySelector(".dance").getElementsByTagName('img')[0].style.visibility="hidden";
     isgameover = false;
     document.querySelector('.line').style.width="0vw";
 
